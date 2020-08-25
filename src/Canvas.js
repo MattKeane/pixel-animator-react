fe1be00bab7b09ca9d75b9981c5a49078808b1ce
Matt Keane <mattpkeane@gmail.com>
2020-08-25 14:25:00 -0500

    Canvas context works

--- src/Canvas.js
+++ src/Canvas.js
@@ -1,9 +1,21 @@
-import React from "react"
+import React, { useRef, useEffect } from "react"
 
 export default function Canvas() {
+
+	const canvas = useRef(null)
+
+	useEffect( () => {
+		const ctx = canvas.current.getContext("2d")
+		ctx.beginPath()
+		ctx.rect(10, 10, 10, 10)
+		ctx.fillStyle = "#000"
+		ctx.fill()
+	})
+
 	return (
-		<React.Fragment>
-			This is the Canvas element.
-		</React.Fragment>
+		<canvas 
+			height="200px" 
+			width="200px"
+			ref={ canvas } />
 	)
 }
