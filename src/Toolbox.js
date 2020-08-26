4d08eba7a6183ff69736c5f617fbbe2ef08a1eba
Matt Keane <mattpkeane@gmail.com>
2020-08-25 20:17:42 -0500

    added buttons and input to control the current frame

--- src/Toolbox.js
+++ src/Toolbox.js
@@ -1,9 +1,40 @@
-import React from "react"
+import React, { useState } from "react"
+import { Button, Input } from "semantic-ui-react"
 
 export default function Toolbox() {
+
+	const [currentFrame, setCurrentFrame] = useState(0)
+
+	function switchToPrevious() {
+		if (currentFrame > 0) {
+			setCurrentFrame(currentFrame - 1)
+		}
+	}
+
+	function switchToNext() {
+		if (currentFrame < 63) {
+			setCurrentFrame(currentFrame + 1)
+		}
+	}
+
+	function handleChange(e) {
+		if (e.target.value >= 1 && e.target.value <= 64) {
+			setCurrentFrame(e.target.value - 1)
+		}
+	}
+
 	return (
-		<React.Fragment>
-			This is the toolbox component.
-		</React.Fragment>
+		<div>
+			<Button 
+				icon="angle left"
+				onClick={ switchToPrevious } />
+			<Input 
+				className="numberInput"
+				value={ currentFrame + 1}
+				onChange={ handleChange } />
+			<Button 
+				icon="angle right"
+				onClick={ switchToNext } />
+		</div>
 	)
 }
